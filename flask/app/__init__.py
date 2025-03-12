86ac93b7343acfcd601b3acf64faa19b31982275
Adam Vanecek <160487894+vaneceka@users.noreply.github.com>
2025-03-12 20:37:43 +0100

    zmena struktury

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,8 +1,6 @@
 import os
 from flask import Flask
-from app.modules.auth.controllers import auth
-from app.modules.dashboard.controllers import dashboard
-from app.modules.api.controllers import api
+from app.controllers.auth_controller import auth
 from app.controllers.main import main
 
 from flask_sqlalchemy import SQLAlchemy
@@ -23,11 +21,11 @@ def create_app():
     db.init_app(app)
     login_manager.init_app(app)
 
-    from app.modules.auth.models import User  
+    from app.models import user_model  
 
     @login_manager.user_loader
     def load_user(user_id):
-        return User.query.get(int(user_id))  # Funkce, která získá uživatele podle ID
+        return user_model.query.get(int(user_id))  # Funkce, která získá uživatele podle ID
     
     with app.app_context():
         db.create_all()  # Vytvoří tabulky, pokud neexistují
@@ -35,7 +33,4 @@ def create_app():
     # Registrace blueprintů
     app.register_blueprint(main)
     app.register_blueprint(auth, url_prefix='/auth')
-    app.register_blueprint(dashboard, url_prefix='/')
-    app.register_blueprint(api, url_prefix='/api')
-
     return app
--- flask/app/modules/dashboard/controllers.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-dashboard = Blueprint('dashboard', __name__)
-
-@dashboard.route('/')
-def home():
-    return "Hlavní stránka"
