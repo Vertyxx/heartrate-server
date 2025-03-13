4d5391b2e60632b4ba82067c45ad8401d5a6915e
Petr <petr.schopp2@seznam.cz>
2025-03-13 14:08:43 +0100

    hashovana hesla SQL

--- sql/test_data.sql
+++ sql/test_data.sql
@@ -1,12 +1,14 @@
 -- Vložení testovacího pacienta
+    -- Heslo: heslo123
 INSERT INTO Pacient (datum_narozeni, jmeno, prijmeni, narodnost, titul, email, heslo, telefon)
 VALUES 
-('1990-05-15', 'Jan', 'Novák', 'Česká', 'Bc.', 'jan.novak@example.com', 'heslo123', '+420123456789');
+('1990-05-15', 'Jan', 'Novák', 'Česká', 'Bc.', 'jan.novak@example.com', 'pbkdf2:sha256:1000000$Qlf17t9cYnbeLdhH$7115bc786ba45a0b8799243a92964e8695caffea120f0553f25576b1544355f5', '+420123456789');
 
 -- Vložení testovacího lékaře
+    -- Heslo: tajneheslo
 INSERT INTO Lekar (datum_narozeni, jmeno, prijmeni, narodnost, zamereni, email, heslo, telefon)
 VALUES 
-('1980-03-22', 'Petr', 'Svoboda', 'Česká', 'Kardiologie', 'petr.svoboda@example.com', 'tajneheslo', '+420987654321');
+('1980-03-22', 'Petr', 'Svoboda', 'Česká', 'Kardiologie', 'petr.svoboda@example.com', 'pbkdf2:sha256:1000000$bGrfhvtKDUsItA25$30f7650aebfbf9247f5c301a99d794aa49b53690f81326ee9ec0646dd26650ff', '+420987654321');
 
 -- Přiřazení pacienta k lékaři (potřebujeme ID)
 INSERT INTO Pacient_Lekar (pacient_id, lekar_id, stav)
