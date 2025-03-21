6aae6355ac9e32d3241d4a683c972fe7f3d6b823
Adam <vanecek@students.zcu.cz>
2025-03-21 16:36:43 +0100

    uprava pro databazi

--- flask/config.py
+++ flask/config.py
@@ -3,12 +3,20 @@ import os
 class Config:
     DEBUG = True 
 
-    DB_USERNAME = os.getenv("DB_USERNAME", "uzivatel")  # Hodnota z docker-compose.yml
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "heslo")
-    DB_HOST = os.getenv("DB_HOST", "mariadb")  # Název služby v Docker Compose
-    DB_NAME = os.getenv("DB_NAME", "HeartRate")
+    # DB_USERNAME = os.getenv("DB_USERNAME", "uzivatel")  # Hodnota z docker-compose.yml
+    # DB_PASSWORD = os.getenv("DB_PASSWORD", "heslo")
+    # DB_HOST = os.getenv("DB_HOST", "mariadb")  # Název služby v Docker Compose
+    # DB_NAME = os.getenv("DB_NAME", "HeartRate")
+
+    # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:3306/{DB_NAME}"
+
+    DB_USERNAME = os.getenv("DB_USERNAME", "root")  # Defaultně použije "root"
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "root")  # Defaultně použije "root"
+    DB_HOST = os.getenv("DB_HOST", "147.228.64.42")  # IP adresa databáze
+    DB_NAME = os.getenv("DB_NAME", "HeartRate")  # Název databáze
 
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:3306/{DB_NAME}"
+
 
     # Zakázání sledování změn objektů pro lepší výkon
     SQLALCHEMY_TRACK_MODIFICATIONS = False
